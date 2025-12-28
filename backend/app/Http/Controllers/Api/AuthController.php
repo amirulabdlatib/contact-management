e98851b7cb7080d2f32c51f55676a5e3d0b3a6bc
Amirul <amirulal73@gmail.com>
2025-12-28 18:48:02 +0800

    Add auth controller and routes

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        return response()->json([
+            'message' => "Register successful",
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        return response()->json([
+            'message' => "Login successful",
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        return response()->json([
+            'message' => "Logout successful",
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
