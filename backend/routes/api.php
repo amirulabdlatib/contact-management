bf0bd5171dc5751d6865875af17bb5cc1ceee168
Amirul <amirulal73@gmail.com>
2025-12-28 20:25:03 +0800

    Moved

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,8 +9,4 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::post('/logout', [AuthController::class, 'logout']);
 });
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
