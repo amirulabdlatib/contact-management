4e5244b0268304fa30b39bbbe0c95f0f9f012d70
Amirul <amirulal73@gmail.com>
2025-12-28 18:37:06 +0800

    Add config

--- backend/config/app.php
+++ backend/config/app.php
@@ -53,7 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-
+    'FRONTEND_URL' => env('FRONTEND_URL'),
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
