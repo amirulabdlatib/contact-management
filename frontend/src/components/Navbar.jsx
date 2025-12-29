ad2244c9c3fdeb6a74b29ad8502738da3c90eb6c
amirulabdlatib <amirulal73@gmail.com>
2025-12-29 14:25:24 +0800

    Update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,33 @@
+// frontend\src\components\Navbar.jsx
+
 import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 const Navbar = () => {
+    const { user, loading } = useAuth();
+
+    if (loading) {
+        return <nav>Loading...</nav>;
+    }
+
     return (
-        <>
-            <nav className="flex items-center justify-between">
-                <Link to="/">Contact app</Link>
-                <div className="space-x-2 mr-2">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
-            </nav>
-        </>
+        <nav className="flex items-center justify-between">
+            <Link to="/">Contact app</Link>
+            <div className="space-x-2 mr-2">
+                {user ? (
+                    <>
+                        <Link to="/dashboard">Dashboard</Link>
+                        <span>Hi, {user.name}</span>
+                        <button>Logout</button>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
+            </div>
+        </nav>
     );
 };
 
