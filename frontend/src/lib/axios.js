9e968491d4ee5f15fc04c760772aa6be04411470
amirulabdlatib <amirulal73@gmail.com>
2025-12-29 12:34:29 +0800

    Add axios.js

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL,
+    headers: {
+        Accept: "application/json",
+    },
+    withCredentials: true,
+});
+
+export default api;
